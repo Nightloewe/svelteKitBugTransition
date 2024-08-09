e3d6bb86b477dcb95d743ac293b8fa226579c840
Nightloewe <nightloewe@gmail.com>
2024-08-09 10:42:20 +0200

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { navigating } from "$app/stores"
+</script>
+{#if $navigating}
+    Lade...
+{:else}
+    <slot></slot>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { fly } from 'svelte/transition'
+</script>
+
+<div style="height: 100%;" transition:fly={{y:200, duration: 500}}>
+    Test
+</div>
+<a href="/test">Test</a>
